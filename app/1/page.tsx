3134971402cc2a6dbde79538a966e307b03bd101
dwaynissh <dwaynissh@gmail.com>
2024-07-25 15:09:36 +0100

    first push

---
+++ app/1/page.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import React, { useContext, useEffect, useState } from "react";
+import { QuestionPart } from "../components/QuestionPart";
+import { DisplayPart } from "../components/DisplayPart";
+import data from "../data/data.json";
+import { usePathname } from "next/navigation";
+import { Context } from "../global/provider";
+
+const page = () => {
+  const { entry, setEntry } = useContext(Context);
+  const value = false;
+  const [text, setText] = useState<string>("");
+
+  const path = usePathname();
+  const val = path.replace("/", "");
+
+  let showPath = 0;
+
+  if (val === "") {
+    showPath = 0;
+  } else {
+    showPath = parseInt(val);
+  }
+
+  useEffect(() => {
+    setEntry(false);
+  }, []);
+
+  return (
+    <div className="min-h-[calc(100vh-20px)] ">
+      <div className="grid w-full grid-cols-1 lg:grid-cols-7 gap-4 h-[calc(100vh-20px)]">
+        <QuestionPart
+          answer={data[showPath!]?.answer}
+          question={data[showPath!]?.question}
+          insight={data[showPath!]?.insight}
+          setText={setText}
+          text={text}
+          showPath={showPath}
+        />
+
+        <div className="h-[500px] order-1 lg:order-2 col-span-1 lg:col-span-3 p-4">
+          <DisplayPart value={entry} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default page;
